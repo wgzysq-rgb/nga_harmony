b63d9f7385e1ed798beb2cac7abc59becf01a67d
ATX_1000 <apap6628114@gmail.com>
2026-04-27 19:39:50 +0800

    chore: 删除废弃的代码文件和资源

--- nga-oh/src/server/lib/json-res.ts
+++
@@ -1,6 +0,0 @@
-import { ServerResponse } from 'node:http';
-
-export function jsonRes(res: ServerResponse, status: number, data: unknown) {
-  res.writeHead(status, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(data));
-}
--- nga-oh/src/shared/types/api.ts
+++
@@ -1,5 +0,0 @@
-export interface ApiResponse<T = any> {
-  ok: boolean;
-  data?: T;
-  error?: string;
-}
